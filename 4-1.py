561a43366d1b983d864d76e5041f831d3ae24d23
kimi2005 <77959769+kimi2005@users.noreply.github.com>
2021-01-28 13:47:56 +0800

    Add files via upload

---
+++ 4-1.py
@@ -0,0 +1,21 @@
+# 4-1
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import random
+
+x,y,z = mc.player.getTilePos()
+
+for i in range(20):
+    r = random.randrange(1,5)
+    if r == 1:
+        mc.setBlocks(x,y,z,x,y,z+4,41)
+        z = z+4
+    if r == 2:
+        mc.setBlocks(x,y,z,x,y,z-4,41)
+        z = z-4
+    if r == 3:
+        mc.setBlocks(x,y,z,x+4,y,z,41)
+        x = x+4
+    if r == 4:
+        mc.setBlocks(x,y,z,x-4,y,z,41)
+        x = x-4
---
+++ 4-4.py
@@ -0,0 +1,19 @@
+# 4-4 
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+list2 = [[12,41,14],[35,73,86]]
+
+myID = mc.getPlayerEntityId("yuankimi2005")
+x,y,z = mc.entity.getTilePos(myID)
+
+startX = x
+
+for i in list2:
+    for j in i:
+        
+        mc.setBlock(x,y,z,j)
+        x = x+1
+    x = startX
+    z = z-1
+    
